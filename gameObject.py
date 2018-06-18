a3b6d7b211b39fd286dd0554ba487719c8b094e1
Amit <ak9067007327@gmail.com>
2018-06-19 01:16:10 +0530

    gameObject.py is created

--- gameObject.py
+++ gameObject.py
@@ -1,5 +1,11 @@
 class gameobject:
-    def __init__(self):
-        self.property ={}
-        self.position = {}
-        self.velocity = {}
+    def __init__(self,name='object'):
+        self.property ={'name':name,'health':100,'point':0}
+        self.position = {'x':None,'y':None}
+        self.velocity = {'vx':None,'y':None}
+    
+    def set_position(self,x=0,y=0):
+        self.position['x'],self.position['y'] = x,y 
+    
+    def set_velocity(self,vx=0,vy=0):
+        self.velocity['vx'],self.velocity['vy'] = vx,vy
