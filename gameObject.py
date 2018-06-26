9d8bc5957bde3677fd22263f4598b0fc465fb650
Amit <ak9067007327@gmail.com>
2018-06-27 02:45:37 +0530

    new files added

---
+++ gameObject.py
@@ -0,0 +1,6 @@
+class gameObject:
+    def __init__(self,name='object',health=100):
+        self.property = {'name':name,'health':health}
+    
+    def add_property(self,property,value):
+        self.property[property] = value
